bc0f023855fd796f9d45928507ff34c9790704e7
Nikolay Nikolov <nikolay86bg@gmail.com>
2019-06-25 23:37:23 +0300

    scraper almost done

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <?php
 //composer require guzzlehttp/guzzle
-//composer require guzzle/plugin-cookie ????
 // composer require symfony/dom-crawler
-// composer require symfony/css-selector ???
+// composer require symfony/css-selector
 
 //http://docs.guzzlephp.org/en/stable/
 //https://symfony.com/doc/current/components/dom_crawler.html
-//composer require symfony/css-selector
 require 'vendor/autoload.php';
 
 use Symfony\Component\DomCrawler\Crawler;
+
+function getMenuCategoriesUrl($html)
+{
+    $crawler = new Crawler($html);
+    return $crawler->filter('.sub-menu > li')->each(function (Crawler $node, $i) {
+        $categoryUrl = $node->filter('a')->attr('href');
+        if (strpos($categoryUrl, '#') !== false) {
+
+        } else {
+            return $categoryUrl;
+        }
+    });
+}
+
 
 $today = new \DateTime();
 
@@ -17,48 +29,88 @@ $loginArray = [
     'email' => 'petarivanov2012@gmail.com',
     'password' => '123456789',
 ];
-$url = 'https://skyphone.bg/login';
+$domain = 'https://skyphone.bg';
+$loginUrl = $domain . '/login';
 $client = new \GuzzleHttp\Client(['cookies' => true]);
-$response = $client->request('POST', $url, [
-    'form_params' =>$loginArray
+$response = $client->request('POST', $loginUrl, [
+    'form_params' => $loginArray
 ]);
 
-$html = ''.$response->getBody();
-// go get data from url
-if($response->getStatusCode() == 200){
-    echo "Logged in!!!";
+$productUrls = [];
 
-//    $response = $client->request('GET', 'https://test.com/level');
-//    $html = ''.$response->getBody();
-//    echo $html;
+// go get category data from url
+if ($response->getStatusCode() == 200) {
+    echo "<h2>Logged in!!!</h2>";
 
-    $crawler = new Crawler($html);
-    $nodeValues = $crawler->filter('.sub-menu > li')->each(function (Crawler $node, $i) {
-        $categoryUrl = $node->filter('a')->attr('href');
-        if (strpos($categoryUrl, '#') !== false) {
+    foreach (getMenuCategoriesUrl('' . $response->getBody()) as $key => $url) {
+        if ($url) {
+            //Go through category paginator and stop if there are no more than 99 results on current page
+            for ($i = 1; $i < 10; $i++) {
+                //get category content
+                $response = $client->request('GET', $domain . $url . $i);
+                $crawler = new Crawler('' . $response->getBody());
 
-        }else{
-            return $categoryUrl;
+                $urls = $crawler->filter('.row-stripedd')->each(function (Crawler $node, $i) {
+                    $urlArray = explode('/', $node->filter('a')->attr('href'));
+                    return '/' . $urlArray[1] . '/' . $urlArray[2] . '/' . $urlArray[3] . '/';
+                });
+
+                $productUrls = array_merge($productUrls, array_flip($urls));
+
+                //if there are no more pages
+                if ($crawler->filter('.row-stripedd')->count() < 99) {
+                    break;
+                }
+            }
         }
-    });
-echo '<pre>';
-    print_R($nodeValues);
-//    $file = fopen('links'.$today->format('Ymd').'.csv', 'w');
-//
-//    foreach($nodeValues as $node){
-//        fputcsv($file,$node);
-//    }
-//
-//    fclose($file);
-//
-//    echo 'CSV created!';
+
+//        if ($key == 1) {
+//            break;
+//        }
+    }
+
+    print_R($productUrls);
+    exit;
 
 
+    $file = fopen('scraped'.$today->format('Ymd').'.csv', 'w');
 
-}else{
-    echo 'Status Code: '.$response->getStatusCode();
+    // go get product data from url
+    foreach ($productUrls as $url => $someValue) {
+        $urlArray = explode('/',$url);
+        $response = $client->request('GET', $domain . $url);
+        $crawler = new Crawler('' . $response->getBody());
+
+        $id = $urlArray[2];
+        $name = trim($crawler->filter('h3')->text());
+        $price = trim($crawler->filter('.dprice')->count() == 1 ? $crawler->filter('.dprice')->text() : $crawler->filter('.price-box')->text());
+        $description = trim($crawler->filter('.product-description')->text());
+        $image = $crawler->filter('.product-full-image')->attr('src');
+
+        if($crawler->filter('.sold')->count() == 1){
+            $inStock = $crawler->filter('.sold')->text();
+        }elseif($crawler->filter('.sold-out')->count() == 1){
+            $inStock = $crawler->filter('.sold-out')->text();
+        }else{
+            $inStock = "";
+        }
+
+        $categories = $crawler->filter('.breadcrumb > li')->each(function (Crawler $node, $i) {
+            return trim($node->filter('a')->text());
+        });
+
+        fputcsv($file,[$id,$name,$price,$description,$image,$inStock,implode(' | ',$categories)]);
+    }
+
+    fclose($file);
+
+    echo '<h2>CSV created!!!</h2>';
+    $end = new \DateTime();
+    echo "Execution time: ";
+    echo $end->diff($today)->format("%i minutes, %s seconds");
+
+} else {
+    echo 'Status Code: ' . $response->getStatusCode();
 }
 
-exit;
-//loop through the data
 
